273cb4569d8e4633895a4bedcb4327f2b8a99f66
Tibor Paczari <paczari.tibor@gmail.com>
2017-02-20 22:10:54 +0100

    This is My first practice project witch is modified

--- app/src/main/java/com/example/android/sabrecounter/MainActivity.java
+++ app/src/main/java/com/example/android/sabrecounter/MainActivity.java
@@ -6,68 +6,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.R.attr.transformPivotY;
+
 public class MainActivity extends AppCompatActivity {
+
 
     int scorePlayerA = 0;
     int scorePlayerB = 0;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-        {
+        if (actionBar != null) {
             actionBar.hide();
         }
+
+        if (savedInstanceState != null) {
+            scorePlayerA = savedInstanceState.getInt("MyScorePlayerA");
+            scorePlayerB = savedInstanceState.getInt("MyScorePlayerB");
+        }
     }
-
-
-    public void displayForPlayerA(int score)
-    {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the data
+        outState.putInt("MyScorePlayerA", scorePlayerA);
+        outState.putInt("MyScorePlayerB", scorePlayerB);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // restore the data
+        scorePlayerA = savedInstanceState.getInt("MyScorePlayerA");
+        scorePlayerB = savedInstanceState.getInt("MyScorePlayerB");
+        UpdateScreen();
+    }
+    private void UpdateScreen() {
+        // update the screen after rotating the phone
+        displayForPlayerA(scorePlayerA);
+        displayForPlayerB(scorePlayerB);
+    }
+    private void displayForPlayerA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.PlayerAScore);
         scoreView.setText(String.valueOf(score));
     }
-
-    public void displayForPlayerB(int score)
-    {
+    private void displayForPlayerB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.PlayerBScore);
         scoreView.setText(String.valueOf(score));
     }
-
-    public void addTOneForPlayerA(View v)
-    {
+    public void addTOneForPlayerA(View v) {
         scorePlayerA++;
         displayForPlayerA(scorePlayerA);
         if (scorePlayerA >= 15)
             displayGameOver("Player A Wins!");
 
     }
-
-    public void addTOneForPlayerB(View v)
-    {
+    public void addTOneForPlayerB(View v) {
         scorePlayerB++;
         displayForPlayerB(scorePlayerB);
         if (scorePlayerB >= 15)
             displayGameOver("Player B Wins!");
     }
-
-    public void resetScore(View v)
-    {
+    public void resetScore(View v) {
         scorePlayerA = 0;
         scorePlayerB = 0;
         displayGameOver("");
         displayForPlayerA(scorePlayerA);
         displayForPlayerB(scorePlayerB);
     }
-
-
-    public void displayGameOver(String text)
-    {
+    private void displayGameOver(String text) {
         TextView t = (TextView) findViewById(R.id.GameOver);
         t.setText(text);
     }
-
 }
